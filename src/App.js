54d5b3374979c66517196d1bf1cfa51571a972b1
plunderwondr <plunderwondr@gmail.com>
2020-03-19 21:10:44 -0400

    Tutorial 12

--- src/App.js
+++ src/App.js
@@ -10,16 +10,16 @@ import Counter from './components/Counter'
 function App() {
   return (
     <div className="App">
-      {/* <Greet name ="Bruce" heroName ="Batman" >
-        <p>I'm an orphan</p>
+        <Greet name ="Bruce" heroName ="Batman" >
+          <p>I'm an orphan</p>
         </Greet>
-      <Greet name ="Dianna" heroName ="WonderWoman"/>
-        <button>Click Me!</button>
-      <Greet name ="Me!" heroName ="Plunderwondr"/>
-      <Welcome name ="Bruce" heroName ="Batman"/>
-      <Welcome name ="Dianna" heroName ="WonderWoman"/>
-      <Hello/>
-      <Message/> */}
+        <Greet name ="Dianna" heroName ="WonderWoman"/>
+         <button>Click Me!</button>
+       <Greet name ="Me!" heroName ="Plunderwondr"/>
+       <Welcome name ="Bruce" heroName ="Batmand"/>
+       <Welcome name ="Dianna" heroName ="WonderWoman"/>
+       <Hello/>
+       <Message/>
       <Counter addValue={2} />
     </div>
   );
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,9 +5,9 @@ import React, { Component } from 'react'
 
 class Welcome extends Component{
     render(){
-        return <h1>Welcome {this.props.name} also known as {this.props.heroName}</h1>
-
-
+        const {name,heroName} = this.props
+        // const {state1, state2} = this.state
+        return <h1>Welcome {name} also known as {heroName}</h1>
     }
 }
 
