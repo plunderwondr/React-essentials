e9891b885083630db9219d65416b97e23418f385
plunderwondr <plunderwondr@gmail.com>
2020-03-26 19:22:41 -0400

    Tutorial 15- Methods as props

--- src/App.js
+++ src/App.js
@@ -6,10 +6,15 @@ import Welcome from './components/Welcome'
 import Hello from './components/Hello'
 import Message from './components/Message'
 import Counter from './components/Counter'
+import EventBind from './components/EventBind'
+import ParentComponent from './components/ParentComponent';
+
 
 function App() {
   return (
     <div className="App">
+      <ParentComponent/>
+      <EventBind/>
         <Greet name ="Bruce" heroName ="Batman" >
           <p>I'm an orphan</p>
         </Greet>
